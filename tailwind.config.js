76c44a36b65da2ce83109fb2afe0234ce10cb893
Vroomvroomzoom <soled.challis.43@icloud.com>
2026-04-29 16:48:36 +0530

    Initial commit: Set up project structure, environment variables, and git configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#006056', /* Dark Teal Green */
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], /* For body text */
+        heading: ['Manrope', 'sans-serif'], /* For headlines */
+      }
+    },
+  },
+  plugins: [],
+}
